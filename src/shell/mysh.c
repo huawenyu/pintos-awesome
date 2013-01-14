19409766ec1f6660414fb8a1a5c36f8ea3772648
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2013-01-14 13:29:28 -0800

    Invoker works for single argument

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -1,4 +1,6 @@
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -22,6 +24,8 @@ char *parse_pipes(char *command, char **arg_strings, size_t *num_arg_strings) {
     *num_arg_strings = 0; 
 
     //TODO (oh god this is annoying)
+
+    return NULL;
 }
 
 /*
@@ -31,16 +35,50 @@ invoke: Takes a command string and forks a child process to run the first comman
 Input:
     - char *command: The command string
 
-Output (int): Zero if success, -1 if something failed
+Output (int): 0 if success, -1 if something failed
 */
 int invoke(char *command) {
-    // TODO: Logic to split the string
+    // TODO: Logic to split the string (use parse_pipes)
 
-    // TODO: Forking logic (and in there, set up file redirects and crap)
+    // Actually do the command and forking etc.
+    //char *arg_strings[] = {"cat", "< blah.txt"};
+    char *arg_strings[] = {"ls"};
+    char *real_argv[] = {"ls", NULL};
+    char *rest = "";
+    if(strcmp(arg_strings[0], "cd") == 0 || strcmp(arg_strings[0], "chdir") == 0) {
+    } else if(strcmp(arg_strings[0], "exit") == 0 ) {
+        exit(EXIT_SUCCESS);
+    } else { // Not an internal command
+        // If the rest is not empty, need to do the pipe stuff TODO
+        if(strlen(rest) != 0) {
+
+        }
+        // Fork!
+        pid_t pid;
+        pid = fork();
+        if(pid == -1) {
+            perror("Fork error");
+            exit(EXIT_FAILURE);
+        } else if(pid == 0) { // Child process
+            // Output indirection TODO
+
+            // Run the process
+            if(execvp(arg_strings[0], real_argv) == -1) {
+                perror("Execvp error");
+            }
+        } else { // Parent
+            int status;
+            waitpid(pid, &status, 0);
+        }       
+    }
 
     // TODO
+
+    return 0;
 }
 
-int main(int argc, char* argv[]) {
-
+int main(int argc, char *argv[]) {
+    invoke("");
+    fprintf(stdout, "Shell successfully invoked\n");
+    return 0;
 }
