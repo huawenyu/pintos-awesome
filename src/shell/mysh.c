e3c4939506a28ead56c678d82a130565b13f0027
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2013-01-15 05:54:51 -0800

    Does quotes correctly. Does pipes maybe correctly?

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -37,6 +37,7 @@ char *parse_pipes(char *command, char **arg_strings, size_t *num_arg_strings) {
     char* last_cmd_ptr = command;
     int idx;
     int num_false_spaces = 0;
+    int inside_quotes = 0;
     for(idx = 0; idx < strlen(command); ++idx) {
         char *cur_cmd_ptr = command + idx;
 
@@ -50,7 +51,8 @@ char *parse_pipes(char *command, char **arg_strings, size_t *num_arg_strings) {
                  strndup(last_cmd_ptr, (int)(cur_cmd_ptr - last_cmd_ptr));
 
              return command + idx; // Pointer to the \0 in command (no 'rest')
-        } else if(*cur_cmd_ptr == ' ') { //TODO: ignore space if inside quotes
+        } else if(*cur_cmd_ptr == ' ' || *cur_cmd_ptr == '\t') { 
+             if(inside_quotes == 1) { continue; }
              // Make sure there are no other delimiting characters before
              // next information (look ahead)
              char *delim_test = cur_cmd_ptr;
@@ -136,6 +138,9 @@ char *parse_pipes(char *command, char **arg_strings, size_t *num_arg_strings) {
              num_false_spaces = 0;
 
             return command+idx+1; // Pointer to one past the pipe
+        } else if (*cur_cmd_ptr == '"') { 
+            if(inside_quotes == 0) { inside_quotes = 1; }
+            if(inside_quotes == 1) { inside_quotes = 0; }
         }
         // Otherwise do nothing -- we're in the middle of a word
 
@@ -151,10 +156,12 @@ piped commands, etc.
 
 Input:
     - char *command: The command string
+    - int pipe_fd: If invoke is being called recursively, the file descriptor
+                   for the read side of the pipe. -1 otherwise.
 
 Output (int): 0 if success, -1 if something failed
 */
-int invoke(char *command) {
+int invoke(char *command, int pipe_fd) {
     // Count the number of pipes
     char *cur_cmd_ptr = command;
     int num_pipes = 0;
@@ -186,6 +193,8 @@ int invoke(char *command) {
         arg_idx = arg_idx - 2;
         num_redir = num_redir + 1;
     }
+
+    // Make the argv array
     char **real_argv = (char **)malloc((num_args+1) * sizeof(char *));
     if(real_argv == NULL) {
         fprintf(stderr, "malloc error\n");
@@ -196,7 +205,17 @@ int invoke(char *command) {
         perror("memcpy error");
         exit(EXIT_FAILURE);
     }
+
     real_argv[arg_idx] = NULL;
+
+    // Strip quotes
+    int i;
+    for(i = 0; i < arg_idx; ++i) {
+        if(real_argv[i][0] == '"') { 
+            char *temp = real_argv[i][0];
+            real_argv[i] = strndup(real_argv[i]+1, strlen(real_argv[i])-2);
+        }
+    }
     if(strcmp(arg_strings[0], "cd") == 0 || 
        strcmp(arg_strings[0], "chdir") == 0) {
         if(num_args != 2) { 
@@ -210,10 +229,6 @@ int invoke(char *command) {
         }
         exit(EXIT_SUCCESS);
     } else { // Not an internal command
-        // If the rest is not empty, need to do the pipe stuff TODO
-        if(strlen(rest) != 0) {
-
-        }
         // Fork!
         pid_t pid;
         pid = fork();
@@ -224,6 +239,30 @@ int invoke(char *command) {
             // Assuming that pipes and I/O redirection aren't being 
             // simultaneously used in a way where something would stomp over 
             // things.
+
+            // If pipe_fd != -1, then this invoke was called from a process
+            // that had stdout to pipe to us and this is the child that
+            // should be reading that.
+            if(pipe_fd != -1) {
+                dup2(pipe_fd, STDIN_FILENO);
+                close(pipe_fd);
+            }
+
+            // Deal with pipes if this process is to output things to
+            // another. In that case, this will be the parent that should
+            // be writing it.
+            if(num_pipes != 0) {
+                int fd[2];
+                if(pipe(fd) < 0) {
+                    perror("pipe");
+                    exit(EXIT_FAILURE);
+                }
+                invoke(rest, fd[0]);
+                close(fd[0]);
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[1]);
+            }
+
             // I/O indirection
             int bracket_idx = num_args - 2;
             int filename_idx = num_args - 1;
@@ -250,12 +289,15 @@ int invoke(char *command) {
                 perror("Execvp error");
             }
         } else { // Parent
+
             int status;
-            // Wait for all piped children
+            // Wait for all piped children, but only if main shell
             int i;
-            for(i = 0; i < num_pipes + 1; ++i) {
-                pid_t w = waitpid(-getgid(), &status, 0);
-                if(w == -1) { perror("waitpid"); exit(EXIT_FAILURE); }
+            if(pipe_fd == -1) {
+                for(i = 0; i < num_pipes + 1; ++i) {
+                    pid_t w = waitpid(-getgid(), &status, 0);
+                    if(w == -1) { perror("waitpid"); exit(EXIT_FAILURE); }
+                }
             }
             for(i = 0; i < num_args; ++i) {
                 free(arg_strings[i]);
@@ -282,7 +324,7 @@ int main(int argc, char *argv[]) {
         fprintf(stdout, "%s@%s:%s$$$ ", uname, host, cwd);
         char buf[1024];
         fgets(buf, 1024, stdin);
-        invoke(buf);
+        invoke(buf, -1);
     }
 
     return 0;
