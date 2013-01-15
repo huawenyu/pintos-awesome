076ba5e70e6e1c27d1a24263c886ca9fe859b04a
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2013-01-14 22:40:29 -0800

    Added some more crap

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 /* 
@@ -46,6 +47,15 @@ Input:
 Output (int): 0 if success, -1 if something failed
 */
 int invoke(char *command) {
+    // Count the number of pipes
+    char *cur_cmd_ptr = command;
+    int num_pipes = 0;
+    while(strchr(cur_cmd_ptr, '|') != NULL)
+    {
+        num_pipes = num_pipes + 1;
+        cur_cmd_ptr = strchr(cur_cmd_ptr, '|') + 1;
+    }
+
     // TODO: Logic to split the string (use parse_pipes)
 
     // Actually do the command and forking etc.
@@ -69,6 +79,10 @@ int invoke(char *command) {
         num_redir = num_redir + 1;
     }
     char **real_argv = (char **)malloc((num_args+1) * sizeof(char *));
+    if(real_argv == NULL) {
+        printf(stderr, "malloc error\n");
+        return -1;
+    }
     if(memcpy((void *)real_argv, (const void*)arg_strings, 
                arg_idx * sizeof(char *)) == NULL) {
         perror("memcpy error");
@@ -77,8 +91,15 @@ int invoke(char *command) {
     real_argv[arg_idx] = NULL;
     if(strcmp(arg_strings[0], "cd") == 0 || 
        strcmp(arg_strings[0], "chdir") == 0) {
-       // TODO change directory
+        if(num_args != 2) { 
+            fprintf(stderr, "cd only takes one parameter\n");
+        }
+        chdir(arg_strings[1]);
     } else if(strcmp(arg_strings[0], "exit") == 0 ) {
+        if(num_args != 1) { 
+            fprintf(stderr, "exit does not take parameters\n");
+            return -1;
+        }
         exit(EXIT_SUCCESS);
     } else { // Not an internal command
         // If the rest is not empty, need to do the pipe stuff TODO
@@ -122,7 +143,11 @@ int invoke(char *command) {
             }
         } else { // Parent
             int status;
-            waitpid(pid, &status, 0);
+            // Wait for all piped children
+            int i;
+            for(i=0; i < num_pipes + 1; ++i) {
+                waitpid(-getgid(), &status, 0);
+            }
             // TODO: Free the strings inside arg_strings 
             // (want to implement parse_pipes first)
             //free(arg_strings);
@@ -130,13 +155,24 @@ int invoke(char *command) {
         }       
     }
 
-    // TODO
-
     return 0;
 }
 
 int main(int argc, char *argv[]) {
-    invoke("");
-    fprintf(stdout, "Shell successfully invoked\n");
+    // Create a new group for later
+    setpgid(0, 0); // Set group to current PID
+    // Loop FOREVER
+    while(1) {
+        char *uname = getlogin();
+        char host[250];
+        gethostname(host, 250);
+        char cwd[250];
+        getcwd(cwd, 250);
+        fprintf(stdout, "%s@%s:%s$$$ ", uname, host, cwd);
+        char buf[1024];
+        fgets(buf, 1024, stdin);
+        invoke(buf);
+    }
+
     return 0;
 }
