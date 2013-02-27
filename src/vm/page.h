4acd79032ec5dad0d31203f8f2189d7c8913c7af
Ilya Nepomnyashchiy <ilyanep@gmail.com>
2013-02-26 22:47:14 -0800

    I need to pay more attention when I commit things

--- src/vm/page.h
+++ src/vm/page.h
@@ -2,14 +2,14 @@
 #define VM_PAGE_H
 
 #include "threads/thread.h"
-#include "lib/kernel/hash.c"
+#include "lib/kernel/hash.h"
 
 enum vm_spt_type {
   SPTE_FS = 001,  // On the file system
   SPTE_SWAP = 002, // In swap 
   SPTE_ZERO = 003, // All zeroes 
   SPTE_MEM = 004 // Actively in memory
-}
+};
 
 struct vm_spte {
   enum vm_spt_type type;
