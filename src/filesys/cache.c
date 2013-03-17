621fe61d82fdcd58ef215ea282335c665055f76e
dychen <dychen@caltech.edu>
2013-03-17 03:58:30 -0700

    Added the buffer cache over the filesystem. See the TODO comments for stuff left to do.

---
+++ src/filesys/cache.c
@@ -0,0 +1,175 @@
+#include "filesys/cache.h"
+#include <list.h>
+#include "devices/block.h"
+#include "filesys/filesys.h"
+#include "threads/malloc.h"
+
+static struct list cache_block_list;
+
+/* TODO: Add synchronization. */
+/* TODO: Initialize the list of blocks. */
+
+/* Returns the block if the desired block is currently in the
+ * cache and null otherwise. */
+struct cache_block * block_in_cache(struct inode *inode, block_sector_t sector_idx) {
+    struct list_elem *e;
+    struct cache_block *c;
+
+    e = list_head(&cache_block_list);
+    while (e != list_end(&cache_block_list) && e != NULL) {
+        c = list_entry(e, struct cache_block, elem);
+        if (c->inode == inode && c->sector_idx == sector_idx) {
+            return c;
+        }
+        e = list_next(e);
+    }
+    return NULL;
+}
+
+// TODO: Update inodes to call this function.
+/* Reads a block from the cache. If it's not currently there, it 
+ * finds the correct block from memory and puts it in the cache, 
+ * evicting another block if necessary.
+ * Returns a pointer to a buffer that can be read from. */
+uint8_t * cache_read(struct inode *inode, block_sector_t sector_idx) {
+    struct cache_block *c;
+    uint8_t *buffer = NULL;
+
+    /* Check if the block is in the cache. */
+    c = block_in_cache(inode, sector_idx);
+    /* If it isn't, find the corresponding data in the filesystem
+     * and allocate a new block for it and put it into the buffer, 
+     * evicting an old block if necessary. */
+    if (c == NULL) {
+        buffer = malloc(BLOCK_SECTOR_SIZE);
+        block_read(fs_device, sector_idx, buffer);
+        // TODO: palloc c?
+        c->inode = inode;
+        c->sector_idx = sector_idx;
+        c->block = buffer;
+        c->count = 0;
+        c->accessed = true;
+        c->dirty = false;
+        if (list_size(&cache_block_list) == CACHE_SIZE) {
+            evict_block();
+            list_push_back(&cache_block_list, &c->elem);
+        }
+    }
+    else {
+        c->accessed = true;
+    }
+    return c->block;
+}
+
+// TODO: Update inodes to call this function.
+/* Writes to a block in the cache. If it's not currently there, it 
+ * finds the correct block from memory and puts it in the cache,
+ * evicting another block if necessary.
+ * Returns a pointer to a buffer that can be written to. */
+uint8_t * cache_write(struct inode *inode, block_sector_t sector_idx) {
+    struct cache_block *c;
+    uint8_t *buffer = NULL;
+
+    /* Check if the block is in the cache. */
+    c = block_in_cache(inode, sector_idx);
+    /* If it isn't, find the corresponding data in the filesystem
+     * and allocate a new block for it and put it into the buffer, 
+     * evicting an old block if necessary. */
+    if (c == NULL) {
+        buffer = malloc(BLOCK_SECTOR_SIZE);
+        /* Reads the data from the disk to the buffer.
+         * Writes happen later. */
+        block_read(fs_device, sector_idx, buffer);
+        // TODO: palloc c?
+        c->inode = inode;
+        c->sector_idx = sector_idx;
+        c->block = buffer;
+        c->count = 0;
+        c->accessed = true;
+        c->dirty = true;
+        if (list_size(&cache_block_list) == CACHE_SIZE) {
+            evict_block();
+            list_push_back(&cache_block_list, &c->elem);
+        }
+    }
+    else {
+        c->accessed = true;
+        c->dirty = true;
+    }
+    return c->block;
+}
+
+/* Writes the data in a buffer to disk. This is called on two 
+ * occasions:
+ * 1. When a block is evicted (in evict_block()).
+ * 2. Periodically when all dirty blocks are written back to disk
+ *    (in buffer_cache_tick()). */
+void cache_write_to_disk(struct cache_block *c) {
+    // TODO: Lock around this.
+    block_write(fs_device, c->sector_idx, c->block);
+    c->dirty = false;
+}
+
+/* Uses an aging replacement policy to find the block to evict. */
+void evict_block(void) {
+    struct list_elem *e;
+    struct cache_block *c;
+    struct cache_block *evict = NULL;
+    int lowest_count;
+
+    e = list_head(&cache_block_list);
+    ASSERT(e != NULL);
+    c = list_entry(e, struct cache_block, elem);
+    lowest_count = c->count;
+    while (e != list_end(&cache_block_list)) {
+        c = list_entry(e, struct cache_block, elem);
+        if (c->count < lowest_count) {
+            lowest_count = c->count;
+            evict = c;
+        }
+        e = list_next(e);
+    }
+
+    ASSERT(evict != NULL);
+    /* Write to filesystem if block was dirty */
+    if (evict->dirty) {
+        cache_write_to_disk(evict);
+    }
+    /* Free memory. */
+    free(evict); // TODO: Does this free the malloc'd evict->block buffer as well?
+}
+
+// TODO: Update the timer to call this function.
+/* 1. Updates the count of each block depending on whether or 
+ *    not it's been accessed since the last timer tick. 
+ * 2. Writes all dirty blocks back to disk. */
+void buffer_cache_tick(int64_t cur_ticks) {
+    struct list_elem *e;
+    struct cache_block *c;
+    bool accessed;
+
+    if (cur_ticks % CACHE_TIMER_FREQ == 0) {
+        e = list_head(&cache_block_list);
+        while (e != list_end(&cache_block_list) && e != NULL) {
+            c = list_entry(e, struct cache_block, elem);
+            accessed = c->accessed;
+            c->count = c->count >> 1;
+            c->count &= (accessed << (sizeof(c->count) - 1));
+            c->accessed = false;
+        }
+        e = list_next(e);
+    }
+
+    // TODO: Lock around this.
+    if (cur_ticks % CACHE_WRITE_ALL_FREQ == 0) {
+        e = list_head(&cache_block_list);
+        while (e != list_end(&cache_block_list) && e != NULL) {
+            c = list_entry(e, struct cache_block, elem);
+            if (c->dirty) {
+                cache_write_to_disk(c);
+            }
+        }
+    }
+}
+
+
---
+++ src/filesys/cache.h
@@ -0,0 +1,52 @@
+#include <stdbool.h>
+#include "filesys/off_t.h"
+#include "devices/block.h"
+#include <list.h>
+
+/* The number of ticks between cache_block count updates. */
+#define CACHE_TIMER_FREQ 100
+/* The number of ticks between periodically writing all 
+ * dirty bits. */
+#define CACHE_WRITE_ALL_FREQ 10000
+/* Maximum number of cache_blocks in the cache. */
+#define CACHE_SIZE 64
+
+struct cache_block {
+    /* In this implementation, the combination of inode
+     * and sector_idx determine the uniqueness of a block. */
+    /* In practice, if you only need sector_idx to determine
+     * the uniqueness of a block, you can delete the following
+     * inode struct and change the block_in_cache() function. */
+    /* The inode the block belongs to. */
+    struct inode *inode;
+    /* The disk sector of the inode. */
+    block_sector_t sector_idx;
+    /* The block of memory allocated for the data. 
+     * For some reason, this is defined as a uint8_t * in 
+     * inode.c (see the bounce buffer), so I left it the 
+     * same here. */
+    uint8_t *block;
+    /* How recently the block has been accessed. */
+    int count;
+    bool dirty;
+    bool accessed;
+
+    struct list_elem elem;
+};
+
+/* Checks if the block is already in the cache. */
+struct cache_block * block_in_cache(struct inode *inode, block_sector_t sector_idx);
+
+/* Evicts a block currently in the cache. */
+void evict_block(void);
+
+uint8_t * cache_read(struct inode *inode, block_sector_t sector_idx);
+uint8_t * cache_write(struct inode *inode, block_sector_t sector_idx);
+/* Writes data from the cache to the disk. This should
+ * happen in the following situations:
+ * 1. When a dirty block is evicted
+ * 2. When the timer calls it (periodically write all 
+ *    dirty blocks back to disk. */
+void cache_write_to_disk(struct cache_block *c);
+
+void buffer_cache_tick(int64_t curr_ticks);
